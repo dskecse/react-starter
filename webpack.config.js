b09f41762882a9ea6cb8bcec44e7247410fe22a8
Dzianis Dashkevich <dskecse@gmail.com>
2016-05-19 14:25:44 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+    devtool: "eval",  // enables sourcemaps, eval is the fastest mode
+    entry: "./src/app.js",
+    output: {
+        path: path.join(__dirname, "static"),
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [    // transformers
+            {
+                test: /\.jsx?$/,
+                loader: "babel",
+                include: path.join(__dirname, "src")
+            }
+        ]
+    }
+};
